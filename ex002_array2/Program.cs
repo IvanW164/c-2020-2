d4e49bf62709a8da2db062a348dff4b341437b08
IvanKratenko <kratenko_ivan@mail.ru>
2022-01-09 22:25:07 +0700

    array2

---
+++ ex002_array2/Program.cs
@@ -0,0 +1,18 @@
+﻿int[] array = { 13, 354, 56, 67, 78, 78, 64, 90 };
+
+int n = array.Length;
+int find = 78;
+
+int index = 0;
+
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+
+    index++;
+
+}
